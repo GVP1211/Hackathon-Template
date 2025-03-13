bae34748af70c6f3b2d52a8ff493344fe09d6fd5
Virunpat Puengrostham <gem.virunpat@gmail.com>
2025-03-13 21:58:22 +0700

    Redid styles and updated readme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import LandingPage from './landing-page.tsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import ClickerGame from './ClickerGame/App.tsx'
 import AiWorkshop from './AIWorkshop/App.tsx'
 import { Provider } from "./components/ui/provider"
+import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
